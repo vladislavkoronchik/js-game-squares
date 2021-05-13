fa392143b4bec23deef0b7ace10f14ba9b84d977
vladislavkoronchik <81505769+vladislavkoronchik@users.noreply.github.com>
2021-05-13 21:10:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+var $time = document.querySelector('#time');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader = document.querySelector('#result-header');
+var $result = document.querySelector('#result');
+var $gameTime = document.querySelector('#game-time');
+
+var score = 0;
+var isGameStarted = false;
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
+
+function show($el) {
+	$el.classList.remove('hide');
+}
+function hide($el) {
+	$el.classList.add('hide');
+}
+
+function startGame() {
+	score = 0;
+	setGameTime();
+	$gameTime.setAttribute('disabled', 'disabled');
+	$gameTime.style.backgroundColor = '#e5e5e5';
+	isGameStarted = true;
+	$game.style.backgroundColor = '#ffffff';
+	hide($start);
+
+	var interval = setInterval(function() {
+		var time = parseFloat($time.textContent);
+
+		if (time <= 0) {
+			clearInterval(interval);
+			endGame();
+		} else {
+			$time.textContent = (time - 0.1).toFixed(1);
+		}
+	}, 100);
+
+	renderBox();
+}
+
+function setGameScore() {
+	$result.textContent = score.toString();
+}
+
+function setGameTime() {
+	var time = +$gameTime.value;
+	$time.textContent = time.toFixed(1);
+	show($timeHeader);
+	hide($resultHeader);
+}
+
+function endGame() {
+	isGameStarted = false;
+	setGameScore();
+	$gameTime.removeAttribute('disabled');
+	$gameTime.style.backgroundColor = '';
+	show($start);
+	$game.innerHTML = '';
+	$game.style.backgroundColor = '';
+	hide($timeHeader);
+	show($resultHeader);
+}
+
+function handleBoxClick(e) {
+	if (!isGameStarted) {
+		return;
+	}
+
+	if(e.target.dataset.box) {
+		score++;
+		renderBox();
+	}
+}
+
+function renderBox() {
+	$game.innerHTML = '';
+
+	var box = document.createElement('div');
+	var boxSize = getRandom(30, 100);
+	var gameSize = $game.getBoundingClientRect();
+	var maxTop = gameSize.height - boxSize;
+	var maxLeft = gameSize.width - boxSize;
+
+	var cr = getRandom(0, 255);
+	var cg = getRandom(0, 255);
+	var cb = getRandom(0, 255);
+	var randomColor = `rgba(${cr}, ${cg}, ${cb})`;
+
+	box.style.height = box.style.width = boxSize + 'px';
+	box.style.backgroundColor = randomColor;
+	box.style.border ='1px solid #000000'
+	box.style.position = 'absolute';
+	box.style.top = getRandom(0, maxTop) + 'px';
+	box.style.left = getRandom(0, maxLeft) + 'px';
+	box.style.cursor = 'pointer';
+	box.setAttribute('data-box', 'true');
+
+	$game.appendChild(box);
+}
+
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
